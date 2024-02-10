bd5a6058d7784520b49d968247f1bc7a94aea473
van.woozoo <van.woozoo@gmail.com>
2024-02-11 01:22:58 +0900

    [Canvas] 조악한 도형 집과 인간

--- picture-app/app.js
+++ picture-app/app.js
@@ -4,9 +4,30 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.lineTo(150, 150);
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
-ctx.stroke();
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+
+ctx.lineWidth = 2;
+ctx.strokeRect(300, 300, 50, 100);
+ctx.fillRect(200, 200, 200, 20);
+
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(450, 200);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "skyblue";
+ctx.fillRect(210, 200, 15, 100);
+ctx.fillRect(350, 200, 15, 100);
+ctx.fillRect(260, 200, 50, 100);
+
+ctx.arc(280, 150, 50, 0,  2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(300, 150, 5, 0, 2 * Math.PI);
+ctx.arc(280, 150, 5, 0, Math.PI, 2 * Math.PI);
+
+ctx.fill();
