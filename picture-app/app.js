ae4ba79357fe7f2369669faa15a48f4b4bb4c0c4
van.woozoo <van.woozoo@gmail.com>
2024-02-10 23:45:26 +0900

    [Canvas] path개념과 활용
    - .gitignore적용

--- picture-app/app.js
+++ picture-app/app.js
@@ -3,3 +3,26 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = 800;
 canvas.height = 800;
+
+ctx.rect(50, 50, 100, 100);
+ctx.rect(150, 150, 100, 100);
+ctx.fill(); 
+ctx.beginPath();
+
+ctx.rect(250, 250, 100, 100);
+/** 
+ fill, stroke선언 윗코드의 모든 캔버스 도형에 적용, 아래 캔버스 도형에 선언을 하지않으면 안보인다.
+-> Path 개념
+ Fill : 색 채우기, Stroke : 선만 있음
+ */
+ 
+ ctx.rect(350, 350, 100, 100);
+ ctx.fillStyle = "red"; // 전체 적용
+ ctx.fill();
+ 
+ ctx.beginPath();
+ ctx.rect(450, 450, 100, 100);
+ ctx.fillStyle = "blue"; 
+ setTimeout(() => {
+    ctx.fill();
+ }, 5000);
